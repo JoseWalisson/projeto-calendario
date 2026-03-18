12bc199c5ea2a20adfe1c71eab9b229c1fab3b0f
José Walisson <102293307+JoseWalisson@users.noreply.github.com>
2026-03-18 17:09:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const botoesContainer = document.getElementById("botoes");
+const imagem = document.getElementById("imagemAtual");
+
+// Criar botões de 1 até 31 automaticamente
+for (let i = 1; i <= 31; i++) {
+    const botao = document.createElement("button");
+    botao.textContent = i;
+
+    botao.addEventListener("click", () => {
+        imagem.src = `img/${i}.jpg`;
+        imagem.alt = `Dia ${i}`;
+
+        // Remove classe ativa de todos
+        document.querySelectorAll("button").forEach(btn => {
+            btn.classList.remove("ativo");
+        });
+
+        // Adiciona no botão clicado
+        botao.classList.add("ativo");
+    });
+
+    botoesContainer.appendChild(botao);
+}
